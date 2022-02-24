a17f9665f17a87c0efd119397e52967042bef441
Maria Clara <mariaclaracesar1_5@hotmail.com>
2022-02-24 17:50:10 -0300

    criando tela

--- App.tsx
+++ App.tsx
@@ -1,20 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return[
+    <SignIn />
+  ]
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { View, 
+  Text, 
+  Image,
+  StatusBar
+} from 'react-native'
+
+import { ButtonIcon } from '../../components/ButtonIcon'
+import IllustrationImg from '../../assets/illustration.png'
+import { styles } from './styles'
+
+export function SignIn() {
+  return(
+    <View style={styles.container}>
+      <StatusBar 
+      barStyle="light-content"
+      backgroundColor="transparent"
+      translucent
+      />
+
+      <Image 
+      source={IllustrationImg} 
+      style={styles.image}
+      resizeMode='stretch'
+      />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>
+       Conecte-se {`\n`}
+       e organize suas {`\n`}
+       jogatinas
+        </Text>
+
+        <Text style={styles.subtitle}>
+        Crie grupos para jogar seus games {`\n`}
+        favoritos com seus amigos
+        </Text>
+
+        <ButtonIcon 
+          title="Entrar com Discord"
+          activeOpacity={0.7}
+        />
+
+      </View>
+    </View>
+  );
+}
